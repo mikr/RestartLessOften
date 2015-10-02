c61d9cea597b5bf1692bc399e53fa8e58a6f868a
Michael Krause <michael@krause-software.com>
2015-10-02 18:11:30 +0200

    Sign the bundle code

--- scripts/rlo_onbuildend.sh
+++ scripts/rlo_onbuildend.sh
@@ -12,15 +12,14 @@ if [ "$PRODUCT_TYPE" = "com.apple.product-type.bundle" -o "$PRODUCT_TYPE" = "com
 	# The bundle build is finished
 
 	if [ "${PLATFORM_NAME}" = "iphoneos" ]; then
-		# Only tell the RLO server that a new bundle exists. The server can then send the bundle to the device.
-
-	    # code signing is not needed for the new code to run, if this ever changes, try this:
-	    # codesign -f -v -s "${CODE_SIGN_IDENTITY}" "${CODESIGNING_FOLDER_PATH}"
+	    # Starting with iOS 8 we need to sign the bundle code.
+	    codesign -f -v -s "${CODE_SIGN_IDENTITY}" "${CODESIGNING_FOLDER_PATH}"
 	    BUNDLE_LOCATION="${BUILT_PRODUCTS_DIR}/${FULL_PRODUCT_NAME}"
 	    URL="http://localhost:8080/announcebundle"
 	    read RLObuildstart 2>/dev/null < ${PROJECT_TEMP_DIR}/RLObuildstart.txt
 	    # Allow curl to exit with a non-zero code
 	    set +e
+		# Only tell the RLO server that a new bundle exists. The server can then send the bundle to the device.
 	    /usr/bin/curl --fail --silent --data-urlencode "srcroot=${SRCROOT}" --data-urlencode "bundlelocation=${BUNDLE_LOCATION}"  --data-urlencode "rlobuildstart=${RLObuildstart}" "$URL"
 	    set -e
 	    true
